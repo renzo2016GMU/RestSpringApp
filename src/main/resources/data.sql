b663688de6d15468609dea2055407e842038b407
Renzo T <rtejada@gmu.edu>
2017-12-29 12:43:28 -0500

    initial commit of Demo project that uses REST service to allow retrieving data from local database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into booking(booking_name) values ('kris');
+insert into booking(booking_name) values ('jon');
+insert into booking(booking_name) values ('clary');
+insert into booking(booking_name) values ('vega');
